149e6861721aa56455fb70633a5aa30f51a07295
Aleksandr Trofimov <aleksandr.trofimov@selmefy.tech>
2022-10-01 17:39:49 +0300

    HOT-6 Add small Room example with Mapper, DTO, Annotations (Lombok)

--- src/main/java/tech/selmefy/hotel/repository/room/Room.java
+++ src/main/java/tech/selmefy/hotel/repository/room/Room.java
@@ -1,4 +1,21 @@
 package tech.selmefy.hotel.repository.room;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "rooms")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
--- src/main/java/tech/selmefy/hotel/repository/room/RoomRepository.java
+++ src/main/java/tech/selmefy/hotel/repository/room/RoomRepository.java
@@ -1,4 +1,8 @@
 package tech.selmefy.hotel.repository.room;
 
-public interface RoomRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends JpaRepository<Room, Long> {
 }
