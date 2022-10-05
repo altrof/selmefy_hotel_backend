3c15251fa4264fe106d45034ff9308b79171ab3d
Norman Kuusik <norman.kuusik@selmefy.tech>
2022-10-05 23:28:48 +0300

    Delete file example_scripts.sql

--- example_scripts.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO hotel ('selmify deluxe');
-
-INSERT INTO hotel_floor (SELECT id FROM hotel WHERE hotel_name='selmify deluxe');
-
-ALTER TABLE room
-ADD CONSTRAINT room_number_within_limits CHECK (0 < room_number AND room_number < 10);
