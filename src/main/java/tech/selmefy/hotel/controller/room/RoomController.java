fe4588d63f17fb17ba63e4dd678da7c705b26bdd
normankuusik@gmail.com <normankuusik@gmail.com>
2022-10-10 21:03:40 +0300

    Add all changes from assigment one.

--- src/main/java/tech/selmefy/hotel/controller/room/RoomController.java
+++ src/main/java/tech/selmefy/hotel/controller/room/RoomController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import tech.selmefy.hotel.controller.room.dto.RoomDTO;
+import tech.selmefy.hotel.repository.room.Room;
 import tech.selmefy.hotel.service.room.RoomService;
 
 import java.util.List;
@@ -15,7 +16,7 @@ public class RoomController {
     public final RoomService roomService;
 
     @GetMapping("/rooms")
-    public List<RoomDTO> getAllRooms() {
+    public List<Room> getAllRooms() {
         return roomService.getAllRooms();
     }
 }
--- src/main/java/tech/selmefy/hotel/repository/room/Room.java
+++ src/main/java/tech/selmefy/hotel/repository/room/Room.java
@@ -9,7 +9,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity(name = "rooms")
+@Entity(name = "room")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -18,4 +18,52 @@ public class Room {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private Float size;
+
+    private int floor_id;
+
+    private int room_number;
+
+    private int number_of_beds;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Float getSize() {
+        return size;
+    }
+
+    public void setSize(Float size) {
+        this.size = size;
+    }
+
+    public int getFloor_id() {
+        return floor_id;
+    }
+
+    public void setFloor_id(int floor_id) {
+        this.floor_id = floor_id;
+    }
+
+    public int getRoom_number() {
+        return room_number;
+    }
+
+    public void setRoom_number(int room_number) {
+        this.room_number = room_number;
+    }
+
+    public int getNumber_of_beds() {
+        return number_of_beds;
+    }
+
+    public void setNumber_of_beds(int number_of_beds) {
+        this.number_of_beds = number_of_beds;
+    }
 }
