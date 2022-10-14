681305339680e8fc835956473c630f65f90f602e
Aleksandr Trofimov <aleksandr.trofimov@selmefy.tech>
2022-10-14 20:39:30 +0300

    HOT-42 assert test

--- src/test/java/tech/selmefy/hotel/HotelApplicationTests.java
+++ src/test/java/tech/selmefy/hotel/HotelApplicationTests.java
@@ -1,11 +1,14 @@
 package tech.selmefy.hotel;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class HotelApplicationTests {
 	@Test
 	void contextLoads() {
+		assertTrue(true);
 	}
 }
