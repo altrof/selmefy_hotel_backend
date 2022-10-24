64fbf10b5a7fc55e4b311628071fd6f12bfc19be
Norman Kuusik <normankuusik@gmail.com>
2022-10-24 13:47:01 +0300

    Add Person entity.

---
+++ src/main/java/tech/selmefy/hotel/repository/person/Person.java
@@ -0,0 +1,43 @@
+package tech.selmefy.hotel.repository.person;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.sql.Date;
+import java.sql.Timestamp;
+
+@Entity(name = "person")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String identityCode;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private Date dateOfBirth;
+
+    @Column(nullable = false)
+    private Timestamp timeOfRegistration = new Timestamp(System.currentTimeMillis());
+    
+}
