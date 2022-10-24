fcb14c093286d7647e2820d789f716a0e7b9e868
Norman Kuusik <normankuusik@gmail.com>
2022-10-24 14:32:34 +0300

    Add person repository.

--- src/main/java/tech/selmefy/hotel/repository/person/PersonRepository.java
+++ src/main/java/tech/selmefy/hotel/repository/person/PersonRepository.java
@@ -1,8 +1,8 @@
-package tech.selmefy.hotel.repository.room;
+package tech.selmefy.hotel.repository.person;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RoomRepository extends JpaRepository<Room, Long> {
+public interface PersonRepository extends JpaRepository<Person, Long> {
 }
