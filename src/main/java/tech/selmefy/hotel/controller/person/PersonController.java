bc8bc55c534332c66a9a3a2f0dde97819d2e5907
Robert Valdmaa <robert.valdmaa@selmefy.tech>
2022-10-30 00:24:49 +0300

    Added createNewEmployee func

--- src/main/java/tech/selmefy/hotel/controller/person/PersonController.java
+++ src/main/java/tech/selmefy/hotel/controller/person/PersonController.java
@@ -3,8 +3,11 @@ package tech.selmefy.hotel.controller.person;
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import tech.selmefy.hotel.controller.room.dto.PersonDTO;
+import tech.selmefy.hotel.repository.person.Person;
 import tech.selmefy.hotel.service.person.PersonService;
 
 import java.util.List;
@@ -13,9 +16,15 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 public class PersonController {
-    public final PersonService personService;
+    public PersonService personService;
     @GetMapping("/person")
     public List<PersonDTO> getAllPeople() {
         return personService.getAllPeople();
     }
+
+    @PostMapping
+    public void createNewEmployee(@RequestBody Person person) {
+        PersonService.createNewPerson(person);
+    }
+
 }
--- src/main/java/tech/selmefy/hotel/service/person/PersonService.java
+++ src/main/java/tech/selmefy/hotel/service/person/PersonService.java
@@ -1,19 +1,24 @@
 package tech.selmefy.hotel.service.person;
 
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import tech.selmefy.hotel.controller.room.dto.PersonDTO;
+import tech.selmefy.hotel.exception.ApiRequestException;
 import tech.selmefy.hotel.mapper.PersonMapper;
 import tech.selmefy.hotel.repository.person.Person;
 import tech.selmefy.hotel.repository.person.PersonRepository;
+import org.springframework.lang.NonNull;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static tech.selmefy.hotel.validators.ObjectUtilityValidator.IsNullOrEmpty;
+
 @Service
 @AllArgsConstructor
 public class PersonService {
-    public final PersonRepository personRepository;
+    public static PersonRepository personRepository;
 
     public List<PersonDTO> getAllPeople() {
         List<Person> people = personRepository.findAll();
@@ -24,4 +29,25 @@ public class PersonService {
         }
         return personDTOList;
     }
+
+    public static void createNewPerson(@NonNull Person person) {
+        if(IsNullOrEmpty(person.getFirstName()) || IsNullOrEmpty(person.getLastName())) {
+            throw new ApiRequestException("Firstname or lastname can not to be null.");
+        }
+
+        if(IsNullOrEmpty(person.getIdentityCode())) {
+            throw new ApiRequestException("Identity code is required in request body.");
+        }
+
+        if(IsNullOrEmpty(person.getDateOfBirth())) {
+            throw new ApiRequestException("Birthday date should be present.");
+        }
+
+        if(IsNullOrEmpty(person.getTimeOfRegistration())) {
+            throw new ApiRequestException("Birthday date should be present.");
+        }
+
+        personRepository.save(person);
+    }
+
 }
