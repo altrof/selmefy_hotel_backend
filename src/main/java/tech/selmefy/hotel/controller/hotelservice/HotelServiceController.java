a39aca71bb4afd4c88bc77f0409707f6e47b19e5
Norman Kuusik <normankuusik@gmail.com>
2022-11-03 20:33:40 +0200

    Add working service to return all entries from the hotel_service table in the database.

--- src/main/java/tech/selmefy/hotel/controller/hotelservice/HotelServiceController.java
+++ src/main/java/tech/selmefy/hotel/controller/hotelservice/HotelServiceController.java
@@ -1,4 +1,22 @@
 package tech.selmefy.hotel.controller.hotelservice;
 
+import io.swagger.annotations.Api;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import tech.selmefy.hotel.controller.hotelservice.dto.HotelServiceDTO;
+import tech.selmefy.hotel.service.hotelservice.HotelServiceService;
+
+import java.util.List;
+
+@Api(tags = "HotelService")
+@RestController
+@RequiredArgsConstructor
 public class HotelServiceController {
+    public final HotelServiceService hotelServiceService;
+
+    @GetMapping("/hotel_services")
+    public List<HotelServiceDTO> getAllHotelServices() {
+        return hotelServiceService.getAllHotelServices();
+    }
 }
