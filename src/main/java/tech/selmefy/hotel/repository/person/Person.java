201eb1dece968d995524950694cbbfc81e8afd78
Aleksandr Trofimov <aleksandr.trofimov@selmefy.tech>
2022-11-05 23:21:21 +0200

    HOT-66 Remove Date from Person

--- src/main/java/tech/selmefy/hotel/repository/person/Person.java
+++ src/main/java/tech/selmefy/hotel/repository/person/Person.java
@@ -10,7 +10,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.sql.Date;
 import java.sql.Timestamp;
 import java.time.LocalDate;
 
