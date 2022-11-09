9329273dd58d25c4f637f64863c72e5a56338847
Robert Valdmaa <robert.valdmaa@selmefy.tech>
2022-11-09 18:18:13 +0200

    everything working on path ../hotel_service_order.

---
+++ src/main/java/tech/selmefy/hotel/service/hotelserviceorder/HotelServiceOrderService.java
@@ -0,0 +1,28 @@
+package tech.selmefy.hotel.service.hotelserviceorder;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import tech.selmefy.hotel.controller.hotelserviceorder.dto.HotelServiceOrderDTO;
+import tech.selmefy.hotel.mapper.HotelServiceOrderMapper;
+import tech.selmefy.hotel.repository.hotelserviceorder.HotelServiceOrder;
+import tech.selmefy.hotel.repository.hotelserviceorder.HotelServiceOrderRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class HotelServiceOrderService {
+
+    public final HotelServiceOrderRepository hotelServiceOrderRepository;
+
+    public List<HotelServiceOrderDTO> getAllHotelServiceOrders() {
+        List<HotelServiceOrder> hotelServiceOrders = hotelServiceOrderRepository.findAll();
+        List<HotelServiceOrderDTO> hotelServiceOrderDTOList = new ArrayList<>();
+        for (HotelServiceOrder order: hotelServiceOrders) {
+            HotelServiceOrderDTO hotelServiceOrderDTO = HotelServiceOrderMapper.INSTANCE.toDTO(order);
+            hotelServiceOrderDTOList.add(hotelServiceOrderDTO);
+        }
+        return hotelServiceOrderDTOList;
+    }
+}
