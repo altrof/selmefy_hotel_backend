0702125db3ab0206d7a32eee20ae61db83246ebb
Robert Valdmaa <robert.valdmaa@selmefy.tech>
2022-11-10 01:08:04 +0200

    made comments column updatable and insertable.

--- src/main/java/tech/selmefy/hotel/repository/hotelserviceorder/HotelServiceOrder.java
+++ src/main/java/tech/selmefy/hotel/repository/hotelserviceorder/HotelServiceOrder.java
@@ -32,6 +32,6 @@ public class HotelServiceOrder {
     @Column(nullable = false)
     private Long price;
 
-    @Column(nullable = false, insertable = false, updatable = false)
+    @Column(nullable = false)
     private String comments;
 }
