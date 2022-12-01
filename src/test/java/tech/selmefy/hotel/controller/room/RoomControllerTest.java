21f49605ad2274f672993927d50f019b14096362
Norman Kuusik <normankuusik@gmail.com>
2022-12-01 18:28:06 +0200

    Add additional test to RoomServiceTest.

--- src/test/java/tech/selmefy/hotel/controller/room/RoomControllerTest.java
+++ src/test/java/tech/selmefy/hotel/controller/room/RoomControllerTest.java
@@ -1,7 +1,11 @@
 package tech.selmefy.hotel.controller.room;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.BDDMockito;
@@ -20,21 +24,39 @@ import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(MockitoExtension.class)
 class RoomControllerTest {
 
+
+    private List<RoomDTO> roomDTOList = new ArrayList<>();
+    RoomDTO room1 = new RoomDTO(1L, 5F, 1, 1, 5, "REGULAR", true);
+    RoomDTO room2 = new RoomDTO(2L, 10F, 1, 2, 2, "DELUXE", false);
+    RoomDTO room3 = new RoomDTO(3L, 10F, 1, 2, 2, "ECONOMY", false);
+    RoomDTO room4 = new RoomDTO(4L, 10F, 1, 2, 2, "REGULAR", false);
+
     @Mock
     private RoomService roomService;
 
     @InjectMocks
     private RoomController roomController;
 
+
+
+    @BeforeEach
+    public void setup() {
+
+    }
+
+    @AfterEach
+    public void tearDown() {
+        roomDTOList.clear();
+    }
+
     @Test
     void getAllRooms() {
+
         // given
-        RoomDTO room1 = new RoomDTO(1L, 5F, 1, 1, 5, "REGULAR", true);
-        RoomDTO room2 = new RoomDTO(2L, 10F, 1, 2, 2, "DELUXE", false);
-        List<RoomDTO> roomDTOList = new ArrayList<>();
         roomDTOList.add(room1);
         roomDTOList.add(room2);
-
+        roomDTOList.add(room3);
+        roomDTOList.add(room4);
         BDDMockito.given(roomService.getAllRooms()).willReturn(roomDTOList);
 
         // when
@@ -44,11 +66,26 @@ class RoomControllerTest {
         assertEquals(roomDTOList.size(), result.size());
         assertTrue(result.contains(room1));
         assertTrue(result.contains(room2));
-
     }
 
     @Test
     void getRoomsByType() {
+        roomDTOList.add(room1);
+        roomDTOList.add(room4);
+
+        BDDMockito.given(roomService.getRoomsByType("REGULAR")).willReturn(roomDTOList);
+
+        // when
+        List<RoomDTO> result = roomController.getRoomsByType("REGULAR");
+
+        // then
+        assertEquals(roomDTOList.size(), result.size());
+        assertTrue(result.contains(room1));
+        assertTrue(result.contains(room4));
+
+        for (RoomDTO roomDTO : result) {
+            assertEquals("REGULAR", roomDTO.getRoomType());
+        }
     }
 
     @Test
---
+++ src/test/java/tech/selmefy/hotel/service/room/RoomServiceTest.java
@@ -0,0 +1,101 @@
+package tech.selmefy.hotel.service.room;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.query.FluentQuery;
+import tech.selmefy.hotel.controller.room.RoomController;
+import tech.selmefy.hotel.controller.room.dto.RoomDTO;
+import tech.selmefy.hotel.mapper.RoomMapper;
+import tech.selmefy.hotel.repository.room.Room;
+import tech.selmefy.hotel.repository.room.RoomRepository;
+import tech.selmefy.hotel.service.room.type.RoomType;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class RoomServiceTest {
+
+    private List<Room> roomList = new ArrayList<>();
+    Room room1 = new Room(1L, 5F, 1, 1, 5, RoomType.REGULAR, true);
+    Room room2 = new Room(2L, 10F, 1, 2, 2, RoomType.DELUXE, false);
+    Room room3 = new Room(3L, 10F, 1, 2, 2, RoomType.ECONOMY, false);
+    Room room4 = new Room(4L, 10F, 1, 2, 2, RoomType.REGULAR, false);
+
+    List<Long> roomIdList = Arrays.asList(new Long[]{1L, 2L, 3L, 4L});
+    @Mock
+    private RoomRepository roomRepository;
+
+    @Spy
+    private RoomMapper roomMapper;
+
+    @InjectMocks
+    private RoomService roomService;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+        roomList.clear();
+    }
+
+    @Test
+    void getAllRooms() {
+
+        // given
+        roomList.add(room1);
+        roomList.add(room2);
+        roomList.add(room3);
+        roomList.add(room4);
+        BDDMockito.given(roomRepository.findAll()).willReturn(roomList);
+
+        // when
+        List<RoomDTO> result = roomService.getAllRooms();
+
+        // then
+        assertEquals(roomList.size(), result.size());
+
+        // Confirming all rooms are here
+        for (RoomDTO roomDTO : result) {
+            assertTrue(roomIdList.contains(roomDTO.getId()));
+        }
+    }
+
+    @Test
+    void getRoomsByType() {
+    }
+
+    @Test
+    void getRoomById() {
+    }
+
+    @Test
+    void getAvailableRooms() {
+    }
+
+    @Test
+    void getRoomAvailableHistoryByRoomId() {
+    }
+
+    @Test
+    void updateRoomAvailableForBooking() {
+    }
+}
