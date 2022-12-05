9ae1676e43be796fe38dce252ee2aa6a2d10a2a6
Anti Natka <anti.natka@ttu.ee>
2022-12-05 17:41:05 +0200

    RoomType finished, cleaned up code

--- src/main/java/tech/selmefy/hotel/repository/room/RoomCriteriaRepository.java
+++ src/main/java/tech/selmefy/hotel/repository/room/RoomCriteriaRepository.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.stereotype.Repository;
 import tech.selmefy.hotel.controller.room.dto.RoomDTO;
+import tech.selmefy.hotel.service.room.type.RoomType;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
@@ -30,19 +31,15 @@ public class RoomCriteriaRepository {
         roomCriteriaQuery.select(root);
         roomCriteriaQuery.orderBy(cb.asc(root.get(orderBy)));
         if (filterBy.isPresent() && filterValue.isPresent()) {
-            System.out.println("this is declared field: " + RoomDTO.class.getDeclaredField(filterBy.get()).getType());
-            System.out.println("COMPARE is: " + cb.lower(root.get(filterBy.get())).toString());
             // currently if boolean true, then returns all available, if false or anything else will return all unvailable rooms
             if(RoomDTO.class.getDeclaredField(filterBy.get()).getType().equals(Boolean.class)) {
                 boolean filterValueAsBoolean = Boolean.parseBoolean(filterValue.get());
-                System.out.println("BOOLEAN is :" + filterValueAsBoolean);
                 roomCriteriaQuery.select(root).where(cb.equal(root.get(filterBy.get()), filterValueAsBoolean));
             }
+            // only works with exact RoomType
             if(RoomDTO.class.getDeclaredField(filterBy.get()).getType().equals(String.class)) {
-                String filterValueAsString = filterValue.get().toUpperCase();
-                System.out.println("String is : " + filterValueAsString);
-//                roomCriteriaQuery.select(root).where(cb.like(cb.lower(root.get(filterBy.get())), filterValue.get().toLowerCase() + '%'));
-                roomCriteriaQuery.select(root).where(cb.like(cb.upper(root.get(filterBy.get())), filterValueAsString));
+                RoomType filerValueAsRoomType = RoomType.valueOf(filterValue.get());
+                roomCriteriaQuery.select(root).where(cb.equal(cb.upper(root.get(filterBy.get())), filerValueAsRoomType));
             }
             if(RoomDTO.class.getDeclaredField(filterBy.get()).getType().equals(Float.class)) {
                 Float filterValueAsFloat = Float.parseFloat(filterValue.get());
@@ -50,19 +47,8 @@ public class RoomCriteriaRepository {
             }
             else if (RoomDTO.class.getDeclaredField(filterBy.get()).getType().equals(int.class)) {
                 Integer filterValueAsInt = Integer.parseInt(filterValue.get());
-                System.out.println("ELSE is :" + filterValueAsInt);
-                System.out.println("this is declared field: " + RoomDTO.class.getDeclaredField(filterBy.get()));
                 roomCriteriaQuery.select(root).where(cb.lessThan(root.get(filterBy.get()), filterValueAsInt));
             }
-
-            //TODO juhul kui filterby tüübid
-            //     private Long id; ok less than int
-            //    private Float size; OK
-            //    private int floorId; ok less than int
-            //    private int roomNumber; OK less than int
-            //    private int numberOfBeds; OK less than int
-            //    private String roomType;
-            //    private Boolean roomAvailableForBooking; ok
         }
         Query query = entityManager.createQuery(roomCriteriaQuery);
         query.setFirstResult(pageNumber * pageSize);
