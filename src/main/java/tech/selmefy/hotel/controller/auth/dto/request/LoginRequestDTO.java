3ce9d60032e0322b16e9ea915b677b79576c01cb
Aleksandr Trofimov <aleksandr.trofimov@selmefy.tech>
2022-12-06 13:05:48 +0200

    HOT-142 Add Unit tests for AuthService (100% coverage)

--- src/main/java/tech/selmefy/hotel/controller/auth/dto/request/LoginRequestDTO.java
+++ src/main/java/tech/selmefy/hotel/controller/auth/dto/request/LoginRequestDTO.java
@@ -1,10 +1,12 @@
 package tech.selmefy.hotel.controller.auth.dto.request;
 
+import lombok.Builder;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
 
 @Data
+@Builder
 public class LoginRequestDTO {
 	@NotBlank
 	private String username;
