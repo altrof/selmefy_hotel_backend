3f1e1609d9bc79b6dd462473473d36d795021a89
Aleksandr Trofimov <aleksandr.trofimov@selmefy.tech>
2022-12-07 12:18:06 +0200

    HOT-147 Some fixes for running integration tests

--- src/test/java/tech/selmefy/hotel/controller/user_account/UserAccountControllerITest.java
+++ src/test/java/tech/selmefy/hotel/controller/user_account/UserAccountControllerITest.java
@@ -1,12 +1,10 @@
 package tech.selmefy.hotel.controller.user_account;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import tech.selmefy.hotel.AbstractIntegrationTest;
